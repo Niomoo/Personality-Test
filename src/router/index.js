685236bae1e71ce6e4219d65ce2896e156e1ddf2
Niomoo <jennyliu.lyh@gmail.com>
2024-05-12 16:43:58 +0800

    [Feature]Add question and results page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import QuestionPage from '@/views/QuestionPage.vue'
+import ResultsPage from '@/views/ResultsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: QuestionPage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: 'results',
+      component: ResultsPage
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
---
+++ src/views/ResultsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h2>Final Results</h2>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'FinalResults',
+  };
+</script>
